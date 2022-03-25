4280538d7d875f4763734a44b5c7a0b1c49c5ec4
Erick Yondon <eryondon@microsoft.com>
2022-03-24 18:46:53 -0700

    Add more complicated project

---
+++ erdemDevice/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Text2Xml;
+
+var converter = new Text2XmlConverter()
+{
+    XmlTagRoot = "F",
+    XmlTagRow = "R",
+    XmlTagToken = "C",
+    IncludeFirstLine = true,
+    IncludeLastLine = true,
+    EnumerateTokens = false,
+    TextRowSeparator = "\n",
+};
+
+var tempPath = Path.GetTempFileName();
+File.WriteAllText(tempPath, @"Yet another XML file.");
+
+var xmlText = converter.Parse(tempPath);
+Console.WriteLine(xmlText);
